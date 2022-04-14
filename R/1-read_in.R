e6548c8b0f1a4d8f5f4775a9b08bf9a331cc59e0
szeller42 <s.zeller@posteo.net>
2022-04-14 21:06:54 +0200

    :construction: replicate paper's stata code on data manipulation

--- R/1-read_in.R
+++ R/1-read_in.R
@@ -11,12 +11,10 @@ sourceDT <- fread("input/data/bls.data.raw")
 names(sourceDT)
 str(sourceDT)
 
-##replicate manipulation from bls.data.raw.do
-sourceDT[sourceDT == -99] <- NA
-
-## replicate manipulation from bls.data.do
+## replicate manipulation from bls.data.do and bls.data.raw.do
 #drop variables
-dataDT <- sourceDT[, .SD, .SDcols = c("ccode1",
+dataDT <- sourceDT[, .SD, .SDcols = c(
+                                    "ccode1",
                                     "ccode2",
                                     "year",
                                     "cap_1",
@@ -35,10 +33,61 @@ dataDT <- sourceDT[, .SD, .SDcols = c("ccode1",
                                     "polity1",
                                     "polity2")]
 
+#replace NA values with NA
+dataDT[dataDT == -99 | 
+         dataDT == -88 | 
+         dataDT == -77 | 
+         dataDT == -66 |
+         dataDT == -999] <- NA
 
+columns <- c(
+             "ccode1",
+             "ccode2",
+             "year",
+             "cap_1",
+             "cap_2",
+             "majpow1",
+             "majpow2",
+             "alliance",
+             "contig",
+             "distance",
+             "mzongo",
+             "mzmid",
+             "mzmidnm",
+             "mzjoany" 
+             )
+dataDT[, (columns) := lapply(.SD, 
+                             function(x) replace(x, which(x==-9), NA)), 
+       .SDcols = columns]
 
 #generate unique dyad identifier
 dataDT[, dcode := ccode1*1000 + ccode2]
 
 #log of capability ratio
-dataDT[, logcapr := log(max(cap_1, cap_2)/min(cap_1, cap_2))]
+dataDT[, logcapr := log(pmax(cap_1, cap_2)/pmin(cap_1, cap_2))]
+dataDT[, `:=`(cap_1 = NULL,
+              cap_2 = NULL)]
+
+#log distance
+dataDT[, logdist := log(distance)]
+dataDT[, distance := NULL]
+
+#simplify contiguous variable
+dataDT[, dircont := contig < 6]
+dataDT[, contig := NULL]
+
+#simplify major powers
+dataDT[, majpow := majpow1 == 1 | majpow2 == 1]
+dataDT[, `:=` (majpow1 = NULL,
+               majpow2 = NULL)]
+
+#simplify alliance 
+dataDT[, allianced := alliance != 4]
+dataDT[, alliance := NULL]
+
+#change MIDs: dropping any ongoing or joiner MIDs
+dataDT[mzongo == 1 | mzjoany == 1, mzmid := NA]
+dataDT[, `:=`(mzjoany = NULL,
+              mzongo = NULL)]
+
+#lead mzmid
