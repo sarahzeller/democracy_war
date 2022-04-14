91ea059d1ce470855f78a6dee0d717a1438e3d72
szeller42 <s.zeller@posteo.net>
2022-04-14 12:02:41 +0200

    🎉 start general set-up file

---
+++ set-up.R
@@ -0,0 +1,8 @@
+############################################
+#Project: Topics in International Economics
+#Author: Sarah
+#Data: Baliga, S, D.O. Lucaa and T. Sjöström (2011)
+#last edit: 22/04/14
+#############################################
+
+library(data.table)
