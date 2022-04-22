dea1b5efd3cbaaab8dc68b9d2b1d403b4cfb3b79
szeller42 <s.zeller@posteo.net>
2022-04-22 11:53:38 +0200

    :pencil2: adjust tabs

--- R/1-read_in.R
+++ R/1-read_in.R
@@ -14,57 +14,59 @@ str(sourceDT)
 ## replicate manipulation from bls.data.do and bls.data.raw.do
 #drop variables
 dataDT <- sourceDT[, .SD, .SDcols = c(
-                                    "ccode1",
-                                    "ccode2",
-                                    "year",
-                                    "cap_1",
-                                    "cap_2",
-                                    "majpow1",
-                                    "majpow2",
-                                    "alliance",
-                                    "dem1",
-                                    "dem2",
-                                    "contig",
-                                    "distance",
-                                    "mzongo",
-                                    "mzmid",
-                                    "mzmidnm",
-                                    "mzjoany",
-                                    "polity1",
-                                    "polity2")]
+  "ccode1",
+  "ccode2",
+  "year",
+  "cap_1",
+  "cap_2",
+  "majpow1",
+  "majpow2",
+  "alliance",
+  "dem1",
+  "dem2",
+  "contig",
+  "distance",
+  "mzongo",
+  "mzmid",
+  "mzmidnm",
+  "mzjoany",
+  "polity1",
+  "polity2"
+)]
 
 #replace NA values with NA
-dataDT[dataDT == -99 | 
-         dataDT == -88 | 
-         dataDT == -77 | 
+dataDT[dataDT == -99 |
+         dataDT == -88 |
+         dataDT == -77 |
          dataDT == -66 |
          dataDT == -999] <- NA
 
 columns <- c(
-             "ccode1",
-             "ccode2",
-             "year",
-             "cap_1",
-             "cap_2",
-             "majpow1",
-             "majpow2",
-             "alliance",
-             "contig",
-             "distance",
-             "mzongo",
-             "mzmid",
-             "mzmidnm",
-             "mzjoany" 
-             )
-dataDT[, (columns) := lapply(.SD, 
-                             function(x) replace(x, which(x==-9), NA)), 
+  "ccode1",
+  "ccode2",
+  "year",
+  "cap_1",
+  "cap_2",
+  "majpow1",
+  "majpow2",
+  "alliance",
+  "contig",
+  "distance",
+  "mzongo",
+  "mzmid",
+  "mzmidnm",
+  "mzjoany"
+)
+dataDT[, (columns) := lapply(.SD,
+                             function(x)
+                               replace(x, which(x == -9), NA)),
        .SDcols = columns]
 
 #generate unique dyad identifier
-dataDT[, dcode := ccode1*1000 + ccode2]
+dataDT[, dcode := ccode1 * 1000 + ccode2]
 
 #log of capability ratio
-dataDT[, logcapr := log(pmax(cap_1, cap_2)/pmin(cap_1, cap_2))]
+dataDT[, logcapr := log(pmax(cap_1, cap_2) / pmin(cap_1, cap_2))]
 dataDT[, `:=`(cap_1 = NULL,
               cap_2 = NULL)]
 
@@ -81,7 +83,7 @@ dataDT[, majpow := majpow1 == 1 | majpow2 == 1]
 dataDT[, `:=` (majpow1 = NULL,
                majpow2 = NULL)]
 
-#simplify alliance 
+#simplify alliance
 dataDT[, allianced := alliance != 4]
 dataDT[, alliance := NULL]
 
@@ -98,25 +100,23 @@ dataDT[, miss := NULL]
 ##yup, definitely some missing
 # so add all year-dcode combinations
 data_completeDT <- dataDT[CJ(dcode = dcode,
-                          year = year,
-                          unique = T),
-                       on=.(dcode, year)]
+                             year = year,
+                             unique = T),
+                          on = .(dcode, year)]
 #take out all years & combinations which are never used
 #TODO make this a bit shorter & prettier
 #create a dummy for implied missing observations
-data_completeDT[, not_missing := year * !is.na(ccode1)][
-  #find the first and last year of observation for dyad
-                  , `:=`(first_year = ifelse(length(unique(not_missing)) == 1, 
-                                             0, 
-                                             min(not_missing[not_missing > 0])),
-                       last_year = max(not_missing)),
-                by = dcode]
+data_completeDT[, not_missing := year * !is.na(ccode1)][#find the first and last year of observation for dyad
+  , `:=`(first_year = ifelse(length(unique(not_missing)) == 1,
+                             0,
+                             min(not_missing[not_missing > 0])),
+         last_year = max(not_missing)),
+  by = dcode]
 #keep only those which are within the interval
 dataDT <- data_completeDT[year >= first_year &
-                                  year <= last_year][
-                                  ,  `:=`(first_year = NULL,
-                                         last_year = NULL,
-                                         not_missing = NULL)]
+                            year <= last_year][,  `:=`(first_year = NULL,
+                                                       last_year = NULL,
+                                                       not_missing = NULL)]
 dataDT[, mzmid1 := shift(mzmid), by = dcode]
 saveRDS(data_completeDT, "output/data_completeDT.rds")
 rm(data_completeDT)
@@ -125,35 +125,38 @@ rm(data_completeDT)
 ###regime type dummies
 #################################
 #check that correlation is really high
-cor(dataDT$polity1, 
-    dataDT$dem1, 
+cor(dataDT$polity1,
+    dataDT$dem1,
     use = "complete.obs")
-cor(dataDT$polity2, 
-    dataDT$dem2, 
+cor(dataDT$polity2,
+    dataDT$dem2,
     use = "complete.obs")
 
 #drop Polity-III scores, replace them with Polity-IV scores
-dataDT[, `:=`(dem1 = polity1,
-               dem2 = polity2,
-               polity1 = NULL,
-               polity2 = NULL
-                       )]
+dataDT[, `:=`(
+  dem1 = polity1,
+  dem2 = polity2,
+  polity1 = NULL,
+  polity2 = NULL
+)]
 
 # dummy variables for each net-democracy level
 #baseline dummies
-dataDT[, `:=`(d7c1n1 = dem1 < -3,
-              d7c1n2 = -3 >= dem1 & dem1 <= 3,
-              d7c1n3 = dem1 > 3,
-              d7c2n1 = dem2 < -3,
-              d7c2n2 = -3 >= dem2 & dem1 <= 3,
-              d7c2n3 = dem2 > 3,
-              #Mansfield and Snyder (2002) dummies
-              dbisc1n1 = dem1 < -6,
-              dbisc1n2 = dem1 >= -6 & dem1 <= 6,
-              dbisc1n3 = dem1 > 6,
-              dbisc2n1 = dem1 < -6,
-              dbisc2n2 = dem1 >= -6 & dem1 <= 6,
-              dbisc2n3 = dem1 > 6 )]
+dataDT[, `:=`(
+  d7c1n1 = dem1 < -3,
+  d7c1n2 = -3 >= dem1 & dem1 <= 3,
+  d7c1n3 = dem1 > 3,
+  d7c2n1 = dem2 < -3,
+  d7c2n2 = -3 >= dem2 & dem1 <= 3,
+  d7c2n3 = dem2 > 3,
+  #Mansfield and Snyder (2002) dummies
+  dbisc1n1 = dem1 < -6,
+  dbisc1n2 = dem1 >= -6 & dem1 <= 6,
+  dbisc1n3 = dem1 > 6,
+  dbisc2n1 = dem1 < -6,
+  dbisc2n2 = dem1 >= -6 & dem1 <= 6,
+  dbisc2n3 = dem1 > 6
+)]
 
 ## Mzmid does not exist after 2000, so delete all observations after
 dataDT <- dataDT[year <= 2000]
