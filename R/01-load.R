1f88eabb17cb8cf8682c25ee55eff79ac1d72399
szeller42 <s.zeller@posteo.net>
2022-05-17 16:20:26 +0200

    ✨ load data set from stata directly in R

--- R/01-load.R
+++ R/01-load.R
@@ -1,13 +1,14 @@
-############################################
-#Project: Topics in International Economics
-#Author: Sarah
-#Data: Baliga, S, D.O. Lucaa and T. Sjöström (2011)
-#Aim:   Read in data
-#last edit: 22/04/14
-#############################################
-
 source("set-up.R")
+# load data set directly into R
 sourceDT <- fread("input/data/bls.data.raw")
 names(sourceDT)
 str(sourceDT)
 saveRDS(sourceDT, "output/sourceDT.rds")
+
+# load data set from stata
+stata("stata/01bls.data.raw.do")
+stata("stata/02bls.data.do")
+stata("stata/03bls.spline.terms.do")
+
+statafull <- readstata13::read.dta13("output/bls.data.full.dta")
+saveRDS(statafull, "output/statafull.rds")
