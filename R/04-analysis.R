eb5f99680f9ad232d2bd1970e1f704ef0e5a10bf
szeller42 <s.zeller@posteo.net>
2022-05-28 14:37:55 +0200

    🐛 make year-FE implicit

--- R/04-analysis.R
+++ R/04-analysis.R
@@ -9,7 +9,7 @@ dataDT <- as.data.table(readRDS("output/full_data.rds"))
 library(alpaca)
 model1 <- feglm(mzmid1 ~ d7n11s + d7n21s + d7n31s + d7n32s + d7n33s + 
                   allianced + majpow + logcapr + py + `_spline1` + `_spline2` + 
-                  `_spline3` + factor(year) | dcode, 
+                  `_spline3` | dcode + year, 
                 data = dataDT[dmmzmid1 == 0])
 summary(model1,
         type = "sandwich")
