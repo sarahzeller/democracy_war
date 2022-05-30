1aa677f5efc333c71e6ba5def1acf533910ab8e3
szeller42 <s.zeller@posteo.net>
2022-05-30 15:35:15 +0200

    ✨ add D_LiLi to Table 1

--- R/06-tables.R
+++ R/06-tables.R
@@ -1,12 +1,39 @@
 # create summary table for data used in baseline
 model1 <- readRDS("output/model1.rds")
-data <- model1$data[, 1:9]
-names(data) <- c("MID onset",
+# need to merge d7n22 in as well so we have data on LiLi dyads
+used_dataDT <- model1$data
+
+fulldataDT <- as.data.table(readRDS("output/full_data.rds"))
+fulldataDT <- fulldataDT[, c("dcode", "year", "d7n22s")
+                         ][, `:=`(dcode = as.factor(dcode),
+                                  year = as.factor(year))]
+
+used_dataDT <- merge(used_dataDT,
+                     fulldataDT,
+                     by = c("dcode", "year"),
+                     all.x = TRUE)
+rm(fulldataDT)
+
+# eliminate variables which aren't interesting in Table 1
+used_dataDT <- used_dataDT[, c("mzmid1",
+                               "d7n11s",
+                               "d7n21s",
+                               "d7n31s",
+                               "d7n32s",
+                               "d7n33s",
+                               "d7n22s",
+                               "allianced",
+                               "majpow",
+                               "logcapr")]
+
+
+names(used_dataDT) <- c("MID onset",
                  "D_{DiDi}",
                  "D_{LiDi}",
                  "D_{DeDi}",
                  "D_{DeLi}",
                  "D_{DeDe}",
+                 "D_{LiLi}",
                  "Allianced",
                  "MajPow",
                  "LogCapRatio")
@@ -14,7 +41,7 @@ names(data) <- c("MID onset",
 library(stargazer)
 library(tidyverse)
 
-table_one <- stargazer(data = data,
+table_one <- stargazer(data = used_dataDT,
                        omit.summary.stat = "n",
                        title = "Sample description for the baseline model",
                        notes = "\\parbox[t]{10cm}{Observations: 40,786. Summary measures for the 
@@ -24,7 +51,6 @@ table_one <- stargazer(data = data,
                        notes.align = "l",
                        label = "tab:summary")
 # TODO: delete unnecessary backslashes
-# TODO: add D_lili
 # table_one <- table_one %>%
 #   str_replace("\_", "_") %>%
 #   str_replace("\\{", "{") %>%
@@ -47,6 +73,10 @@ rm(paths)
 
 # save list to rds file
 saveRDS(raw_tables, file = "tables/raw_tables.RDS")
+
+# ALTERNATIVE
+# TODO extract only variables of interest
+texreg::texreg(model1)
 
 # write PDF file
 knitr::knit2pdf("R/simple.Rnw", "tables/simple.tex")
