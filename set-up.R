7ce7b19319d1d011af083b4aaf2d70564846a83e
szeller42 <s.zeller@posteo.net>
2022-06-01 17:02:45 +0200

    💄 add custom ggplot theme

--- set-up.R
+++ set-up.R
@@ -6,3 +6,23 @@ library(RStata)
 print("Choose where your stata .exe file lies")
 ifelse(is.null(options("RStata.StataPath")), chooseStataBin(), "Stata path already chosen")
 options("RStata.StataVersion" = 14.2)
+
+
+## Custom theme for ggplot
+library(extrafont)
+loadfonts(device = "win")
+Sys.setenv(R_GSCMD = "C:/Program Files/gs/gs9.55.0/bin/gswin64c.exe")
+ggsave_embed <- function(name){
+  ggsave(name)
+  embed_fonts(name)
+}
+
+custom_theme <- function(){
+  theme_minimal()+
+    theme(text = element_text(family = "DM Sans"),
+          plot.title = element_text(size = 18),
+          plot.subtitle = element_text(size = 13, color = "grey30"),
+          axis.text.y = element_text(size = 10, color = "grey10"),
+          axis.line.x = element_line(colour = "gray40"),
+          panel.grid = element_blank())
+}
