b1aae95b438ab2fc60147b049000b896eaa4a95e
szeller42 <s.zeller@posteo.net>
2022-06-03 11:49:08 +0200

    ✨ add bar chart for regime type frequency

--- set-up.R
+++ set-up.R
@@ -12,8 +12,8 @@ options("RStata.StataVersion" = 14.2)
 library(extrafont)
 loadfonts(device = "win")
 Sys.setenv(R_GSCMD = "C:/Program Files/gs/gs9.55.0/bin/gswin64c.exe")
-ggsave_embed <- function(name, width){
-  ggsave(name, width = width)
+ggsave_embed <- function(name, width, height = NA){
+  ggsave(name, width = width, height)
   embed_fonts(name)
 }
 
@@ -21,8 +21,12 @@ custom_theme <- function(){
   theme_minimal()+
     theme(text = element_text(family = "Open Sans"),
           plot.title = element_text(size = 18),
-          plot.subtitle = element_text(size = 13, color = "grey30"),
-          axis.text.y = element_text(size = 10, color = "grey10"),
+          plot.subtitle = element_text(size = 13, 
+                                       color = "grey50"),
+          axis.text.y = element_text(size = 10, 
+                                     color = "grey10",
+                                     hjust = 0),
+          axis.text.x = element_text(hjust = 0),
           axis.line.x = element_line(colour = "gray40"),
           panel.grid = element_blank())
 }
