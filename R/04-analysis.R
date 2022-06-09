4435510a1e5e056e5b429252a148dbb810def4e1
szeller42 <s.zeller@posteo.net>
2022-06-09 11:47:55 +0200

    ✨ check out subset used in regression

--- R/04-analysis.R
+++ R/04-analysis.R
@@ -1,9 +1,11 @@
 # load data
 require(data.table)
+require(dplyr)
 dataDT <- as.data.table(readRDS("output/full_data.rds"))
 
-
+##############################
 # direct comparison with paper
+###############################
 
 library(alpaca)
 model1 <- feglm(mzmid1 ~ d7n11s + d7n21s + d7n31s + d7n32s + d7n33s + 
@@ -24,3 +26,14 @@ model_only_lili <- feglm(mzmid1 ~ d7n22s + allianced + majpow + logcapr|
 summary(model_only_lili,
         type = "sandwich")
 saveRDS(model1, "output/model_only_lili.rds")
+
+##########################################
+# check out models' subset
+#############################################
+# dyads
+n_distinct(model1$data$dcode)/n_distinct(dataDT$dcode) # 0.03
+# observations
+nrow(model1$data)/nrow(dataDT)  # 0.06
+# years
+n_distinct(model1$data$year)/n_distinct(dataDT$year)
+unique(dataDT$year)[!unique(dataDT$year) %in% unique(model1$data$year)]
