003ffe5ebbdf318e71e2f7f51e4529ea5a7d84d9
szeller42 <s.zeller@posteo.net>
2022-06-16 12:47:05 +0200

    ➕ add grateful library

--- R/07_citations.R
+++ R/07_citations.R
@@ -1,10 +1,16 @@
 library(renv)
 dep <- dependencies()
 packages <- unique(dep$Package)
-# 
+#
 # references <- c(toBibtex(citation()))
 # for(p in 1:length(packages)){
 #   references[[p+1]] <- toBibtex(citation(packages[p]))
 # }
 
 knitr::write_bib(c(packages, "base"), "output/packages.bib")
+
+# library(grateful)
+# cite_packages(output = "paragraph",
+#               out.dir = "output/",
+#               cite.tidyverse = TRUE,
+#               include.RStudio = TRUE)
